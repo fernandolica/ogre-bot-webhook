ac4a97d336c5b4f23a9df7043656afea761211d1
fernandolica <fernando9b@hotmail.com>
2017-08-16 10:03:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    ''' yql_query = makeYqlQuery(req)
-    if yql_query is None:
+    yql_query = makeYqlQuery(req)
+    ''' if yql_query is None:
         return {}
     yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
     result = urlopen(yql_url).read()
