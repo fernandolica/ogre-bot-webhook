ec0024e9f7b562fd489caaa462b4c58ffdcbd357
fernandolica <fernando9b@hotmail.com>
2017-08-16 10:44:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
+    if req.get("result").get("action") != "getIntent":
         return {}
     
     res = makeYqlQuery(req)
